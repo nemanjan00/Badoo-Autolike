c8a37c7357274d6f56c1e0f396981709e28604d0
nemanjan00 <nemanjan00@gmail.com>
2014-12-08 20:59:27 +0100

    v2

--- Badoo-Autolike.user.js
+++ Badoo-Autolike.user.js
@@ -8,7 +8,7 @@
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
 // @namespace https://github.com/nemanjan00/Badoo-Autolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
-// @version 1
+// @version 2
 // ==/UserScript==
 
 var Timer = setInterval(TimerFunction, 50);
