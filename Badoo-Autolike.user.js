199746a859e6458af877b6734cb622c4480d8b94
nemanjan00 <nemanjan00@gmail.com>
2014-12-08 18:31:37 +0100

    Init

--- Badoo-Autolike.user.js
+++ Badoo-Autolike.user.js
@@ -3,6 +3,8 @@
 // @author nemanjan00
 // @include https://badoo.com/*
 // @include https://*.badoo.com/*
+// @include http://badoo.com/*
+// @include http://*.badoo.com/*
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
 // @namespace https://github.com/nemanjan00/Badoo-Autolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
