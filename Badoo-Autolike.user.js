8c642ae5a244f8e1dffcfbf92fa88d962ad675cc
nemanjan00 <nemanjan00@gmail.com>
2014-12-12 14:26:53 +0100

    Bug fix

--- Badoo-Autolike.user.js
+++ Badoo-Autolike.user.js
@@ -8,32 +8,54 @@
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
 // @namespace https://github.com/nemanjan00/Badoo-Autolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
-// @version 2
+// @version 3
 // ==/UserScript==
 
-var Timer = setInterval(TimerFunction, 50);
-var Timer1 = setInterval(RemoveAlert, 50);
+var oldurl = "";
 
-function TimerFunction() {
+var AutoYesService;
+var RemoveAlertService;
+
+var running = 1;
+
+var URLHandlerService = setInterval(URLHandler, 1000);
+
+function URLHandler() {
+	if(window.location.href.indexOf("encounters") > -1 && window.location.href != oldurl) {
+		AutoYesService = setInterval(AutoYes, 50);
+		RemoveAlertService = setInterval(RemoveAlert, 50);
+	}
+	else
+	{
+		if(running == 1){
+			clearInterval(AutoYesService);
+		}
+
+		clearInterval(RemoveAlertService);
+	}
+
+	oldurl = window.location.href;
+}
+
+function AutoYes() {
 	if(window.location.href.indexOf("encounters") > -1) {
 		if(typeof document.getElementsByClassName('wizard_cloud')[0] == "undefined"){
 			document.getElementsByClassName('js-profile-header-vote b-link')[0].click();
 		}
 		else
 		{
-			StopFunction();
+			running = 0;
+			clearInterval(AutoYesService);
 		}
 	}
 }
 
 function RemoveAlert() {
 	if(window.location.href.indexOf("encounters") > -1) {
-		for (i = 0; i < document.getElementsByClassName('js-ovl-close').length; i++) {
-			document.getElementsByClassName('js-ovl-close')[i].click();
+		if(typeof document.getElementsByClassName('wizard_cloud')[0] == "undefined" || running == 0){
+			for (i = 0; i < document.getElementsByClassName('js-ovl-close').length; i++) {
+				document.getElementsByClassName('js-ovl-close')[i].click();
+			}
 		}
 	}
-}
-
-function StopFunction() {
-	clearInterval(Timer);
 }
