4008d35fd38f67caa305d1e62e04a35ea6405e05
Nemanja Nedeljković <nemanjan00@gmail.com>
2015-11-23 14:07:02 +0100

    Bug fix

--- Badoo-Autolike.user.js
+++ Badoo-Autolike.user.js
@@ -8,7 +8,7 @@
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
 // @namespace https://github.com/nemanjan00/Badoo-Autolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/Badoo-Autolike/master/Badoo-Autolike.user.js
-// @version 5
+// @version 6
 // ==/UserScript==
 
 var oldurl = "";
@@ -21,7 +21,7 @@ var running = 1;
 var URLHandlerService = setInterval(URLHandler, 1000);
 
 function URLHandler() {
-	if(window.location.href.indexOf("game") > -1) {
+	if(window.location.href.indexOf("encounters") > -1) {
 		if(window.location.href != oldurl){
 			AutoYesService = setInterval(AutoYes, 50);
 			RemoveAlertService = setInterval(RemoveAlert, 50);
@@ -40,7 +40,7 @@ function URLHandler() {
 }
 
 function AutoYes() {
-	if(window.location.href.indexOf("game") > -1) {
+	if(window.location.href.indexOf("encounters") > -1) {
 		if(typeof document.getElementsByClassName('wizard_cloud')[0] == "undefined" && running == 1){
 			document.getElementsByClassName('js-profile-header-vote b-link')[0].click();
 		}
